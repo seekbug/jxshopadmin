38f0612875ac1dc684f464c14a76912aebc5d650
seekbug <2582866158@qq.com>
2020-10-17 19:31:17 +0800

    init

--- src/router.js
+++ src/router.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
+//配置ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+// 配置 全局初始化样式
+import '@/assets/css/reset.scss';
+
 Vue.use(Router)
 
 export default new Router({
